336250c9613d86109ec6b21eabc0c36196fef046
Filipe Fenrich <foolemdev@gmail.com>
2018-09-05 01:59:43 -0300

    production mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'https://satads.heroku.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :authentication => :plain,
+    :domain => 'satads.heroku.com',
+    :user_name => 'emaildocatei@gmail.com',
+    :password => 'integracao2018'
+}
 end
