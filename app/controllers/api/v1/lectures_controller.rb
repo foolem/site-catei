b164f6c70a791c0d2ee88dca4af620d141e60908
Filipe Fenrich <foolemdev@gmail.com>
2018-09-12 08:57:25 -0300

    Going back

--- app/controllers/api/v1/lectures_controller.rb
+++ app/controllers/api/v1/lectures_controller.rb
@@ -5,7 +5,7 @@ module Api::V1
     def index
       time = (Time.now - 3.hours) # heroku - 3 horas
 
-      @lectures = Lecture.where(day: time.day).order(:created_at)
+      @lectures = Lecture.where(day: time.day)
 
       render json: @lectures, status: :ok
     end
